2a0a85506570be781b7d1cee524fd182b76c7f8f
Sim Tran <sim.tran@katalon.com>
2024-08-08 14:42:47 +0700

    Update

--- Scripts/Windows Keywords/AC-FA0651040 Wait for element not present/Script1718865331497.groovy
+++ Scripts/Windows Keywords/AC-FA0651040 Wait for element not present/Script1718865331497.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-Windows.startApplication(GlobalVariable.KatalonPath, FailureHandling.STOP_ON_FAILURE)
+Windows.startApplication(GlobalVariable.currentProjDir + GlobalVariable.KatalonPath, FailureHandling.STOP_ON_FAILURE)
 
 beforeDelay = System.currentTimeMillis()
 try {
